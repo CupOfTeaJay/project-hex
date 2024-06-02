33a3fa31263c0ccae587a00190478803413734e1
CupOfTeaJay <tjgiven17@protonmail.com>
2024-06-02 12:29:21 -0400

    init tile / unit picking, event by event basis.

--- src/systems/selection/make_tile_pickable.rs
+++ src/systems/selection/make_tile_pickable.rs
@@ -17,18 +17,43 @@
 */
 
 use bevy::prelude::*;
-use bevy_mod_picking::PickableBundle;
+use bevy_mod_picking::prelude::*;
 
 use crate::events::tile_spawn_event::TileSpawnEvent;
 
+// TODO: There's probably a much better way to do all of this.
 /// Makes a tile scene pickable (selectable).
 pub fn make_tile_pickable(
-    mut tile_spawn_event: EventReader<TileSpawnEvent>,
     mut commands: Commands,
+    query_children: Query<&Children>,
+    query_entities: Query<Entity, (With<Handle<Mesh>>, Without<Pickable>)>,
+    mut tile_spawn_event: EventReader<TileSpawnEvent>,
 ) {
     for event in tile_spawn_event.read() {
+        make_meshes_pickable_recursively(
+            &mut commands,
+            &event.entity,
+            &query_children,
+            &query_entities,
+        );
+    }
+}
+
+fn make_meshes_pickable_recursively(
+    commands: &mut Commands,
+    parent: &Entity,
+    query_children: &Query<&Children>,
+    query_entities: &Query<Entity, (With<Handle<Mesh>>, Without<Pickable>)>,
+) {
+    if let Ok(children) = query_children.get(*parent) {
+        for child in children {
+            make_meshes_pickable_recursively(commands, child, query_children, query_entities);
+        }
+    }
+
+    if let Ok(entity_with_mesh) = query_entities.get(*parent) {
         commands
-            .entity(event.entity)
+            .entity(entity_with_mesh)
             .insert(PickableBundle::default());
     }
 }
