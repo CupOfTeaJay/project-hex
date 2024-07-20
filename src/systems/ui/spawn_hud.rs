7c8bf115c49b4e5ed51a4a7d2e9654fbf9f62b83
CupOfTeaJay <tjgiven17@protonmail.com>
2024-07-20 19:45:02 -0400

    left/right hud split

--- src/systems/ui/spawn_hud.rs
+++ src/systems/ui/spawn_hud.rs
@@ -20,20 +20,50 @@ use bevy::prelude::*;
 use bevy_mod_picking::prelude::*;
 
 pub fn spawn_hud(mut commands: Commands) {
-    // Root node.
-    println!("Spawned root node!");
-    commands.spawn((
-        NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                height: Val::Percent(100.0),
-                justify_content: JustifyContent::SpaceBetween,
-                border: UiRect::all(Val::Px(10.0)),
+    // Root node. Encapsulates the entire screen.
+    commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    justify_content: JustifyContent::SpaceBetween,
+                    border: UiRect::all(Val::Px(5.0)),
+                    ..default()
+                },
+                border_color: BorderColor(Color::srgb(1.00, 0.00, 0.00)),
                 ..default()
             },
-            border_color: BorderColor(Color::srgb(1.00, 0.00, 0.00)),
-            ..default()
-        },
-        Pickable::IGNORE,
-    ));
+            Pickable::IGNORE,
+        ))
+        .with_children(|parent| {
+            // Left "pane" (vertical split of root).
+            parent.spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(50.0),
+                        justify_self: JustifySelf::Start,
+                        border: UiRect::all(Val::Px(5.0)),
+                        ..default()
+                    },
+                    border_color: Color::srgb(0.6, 0.0, 0.4).into(),
+                    ..default()
+                },
+                Pickable::IGNORE,
+            ));
+            // Right "pane" (vertical split of root).
+            parent.spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(50.0),
+                        justify_self: JustifySelf::End,
+                        border: UiRect::all(Val::Px(5.0)),
+                        ..default()
+                    },
+                    border_color: Color::srgb(0.6, 0.0, 0.4).into(),
+                    ..default()
+                },
+                Pickable::IGNORE,
+            ));
+        });
 }
