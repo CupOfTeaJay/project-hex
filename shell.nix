af14eee709faab4fcb9c663877649b7164980713
Thomas Given <tjgiven17@protonmail.com>
2024-10-13 12:24:58 -0400

    init shell.nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+/*
+ * TODO:
+ */
+
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+mkShell rec {
+    nativeBuildInputs = [
+        cargo
+        pkg-config
+        rustc
+    ];
+    buildInputs = [
+      udev alsa-lib vulkan-loader
+      xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+      libxkbcommon wayland # To use the wayland feature
+    ];
+    LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+}
+
